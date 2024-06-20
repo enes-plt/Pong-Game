05cf8be6237a430d160980646279eaa70138eeec
Enes <enesfplt@gmail.com>
2024-06-20 00:00:05 -0400

    Pong Game

--- GameFrame.java
+++ GameFrame.java
@@ -12,20 +12,28 @@ public class GameFrame extends JFrame{
 
         // Initialize the game panel
         panel = new GamePanel();
+
         // Add the panel to the frame
         this.add(panel);
+
         // Set the title of the frame
         this.setTitle("Pong Game");
+
         // Disable resizing
         this.setResizable(false);
+
         // Set the background color
         this.setBackground(Color.black);
+
         // Close the application when the frame is closed
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
         // Pack the components within the frame
         this.pack();
+
         // Make the frame visible
         this.setVisible(true);
+        
         // Center the frame on the screen
         this.setLocationRelativeTo(null);
 
--- Score.java
+++ Score.java
@@ -5,26 +5,34 @@ import javax.swing.*;
 
 public class Score {
 
+    // Static variables for game dimensions
     static int GAME_WIDTH;
     static int GAME_HEIGHT;
+
+    // Variables for keeping track of players' scores
     int player1;
     int player2;
-    
-    Score(int GAME_WIDTH, int GAME_HEIGHT){
 
-        Score.GAME_WIDTH = GAME_WIDTH;
-        Score.GAME_HEIGHT = GAME_HEIGHT;
-
+    // Constructor to initialize the score with game dimensions
+    Score(int GAME_WIDTH, int GAME_HEIGHT) {
+        Score.GAME_WIDTH = GAME_WIDTH; // Assign the game width to the static variable
+        Score.GAME_HEIGHT = GAME_HEIGHT; // Assign the game height to the static variable
     }
 
-    public void draw(Graphics g){
+    // Method to draw the score on the screen
+    public void draw(Graphics g) {
+        g.setColor(Color.white); // Set the drawing color to white
+        g.setFont(new Font("Consolas", Font.PLAIN, 60)); // Set the font to "Consolas", plain style, and size 60
 
-        // Draw the score
-        g.setColor(Color.white);
-        g.setFont(new Font("Consolas", Font.PLAIN, 60));
+        // Draw a vertical line at the center of the screen to divide it into two halves
         g.drawLine(GAME_WIDTH / 2, 0, GAME_WIDTH / 2, GAME_HEIGHT);
-        g.drawString(String.valueOf(player1 / 10)+ String.valueOf(player1 % 10), (GAME_WIDTH / 2) - 85, 50);
-        g.drawString(String.valueOf(player2 / 10)+ String.valueOf(player2 % 10), (GAME_WIDTH / 2) + 20, 50);
 
+        // Draw player 1's score on the left side of the screen
+        // The score is split into tens and units for proper formatting
+        g.drawString(String.valueOf(player1 / 10) + String.valueOf(player1 % 10), (GAME_WIDTH / 2) - 85, 50);
+
+        // Draw player 2's score on the right side of the screen
+        // The score is split into tens and units for proper formatting
+        g.drawString(String.valueOf(player2 / 10) + String.valueOf(player2 % 10), (GAME_WIDTH / 2) + 20, 50);
     }
 }
